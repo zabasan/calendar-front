d0b2707fe7c1c422759331efccc4023f3d5da624
Santiago Zabaleta <santiago@dezabaleta.com.ar>
2020-02-09 17:30:27 -0300

    fix repeated

--- src/app/layout/filters/filters.module.ts
+++ src/app/layout/filters/filters.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {FiltersComponent} from './filters.component';
 import {FiltersRoutingModule} from './filters-routing.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule} from '@angular/forms';
@@ -10,6 +9,6 @@ import {PageHeaderModule} from '../../shared';
 
 @NgModule({
     imports: [CommonModule, FiltersRoutingModule, PageHeaderModule, FormsModule, NgbModule, NgxSelectModule],
-    declarations: [FiltersComponent]
+    declarations: []
 })
 export class FiltersModule {}
